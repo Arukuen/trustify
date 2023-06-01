0ec7dfd4a8edb81c0f8149c5f6ba63193a25a842
Arukuen <adsarmiento3@up.edu.ph>
2023-06-01 14:25:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import random
+
+class Text(BaseModel):
+    data: str
+
+app = FastAPI()
+
+def faker_model(text: str) -> bool:
+    random.seed(len(text))
+    return random.randint(0, 10) >= 5
+
+@app.post("/text")
+async def process_text(text: Text):
+    is_fake = faker_model(text.data)
+    response_data = {'fake': is_fake}
+    return response_data
