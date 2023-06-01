98a4b6699572cc74c705e0a56bf40aca2423b50e
Arukuen <adsarmiento3@up.edu.ph>
2023-06-01 16:45:38 +0800

    feat: add alive check

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def faker_model(text: str) -> bool:
     random.seed(len(text))
     return random.randint(0, 10) >= 5
 
+@app.get("/")
+def is_alive():
+    return {"is_alive": True}
+
 @app.post("/text")
 async def process_text(text: Text):
     is_fake = faker_model(text.data)
