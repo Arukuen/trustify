573bd92b2cf4738a617b753cc4e80ca3723e4b39
Arukuen <adsarmiento3@up.edu.ph>
2023-06-04 19:16:50 +0800

    fix: remove unnecessary test functions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import random
 import model_api
 
 class Text(BaseModel):
@@ -8,9 +7,6 @@ class Text(BaseModel):
 
 app = FastAPI()
 
-def faker_model(text: str) -> bool:
-    random.seed(len(text))
-    return random.randint(0, 10) >= 5
 
 @app.get("/")
 def is_alive():
@@ -19,6 +15,5 @@ def is_alive():
 @app.post("/api/text/")
 async def process_text(text: Text):
     is_fake = model_api.predict(text.data)
-    print(is_fake)
     response_data = {"is_fake": is_fake}
     return response_data
