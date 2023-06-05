25b38ff87f92211acaecae646f0b6d35645af24f
Gabriel Samonte <samontegabriel2001@gmail.com>
2023-06-05 22:59:20 +0800

    Add invalid option

--- model_api.py
+++ model_api.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 from model.stops import stop_words
 
 with open('./model/model_pickle', 'rb') as f:
@@ -11,6 +12,11 @@ with open('./model/vect_pickle', 'rb') as f:
 def predict(text):
     text_arr = []
     text_arr.append(text)
-    text_dtm = imported_vect.transform(text_arr)
-    res = imported_model.predict(text_dtm)[0]
-    return bool(res)
+    num_words = len(re.findall(r'\w+', text))
+
+    res = 2
+    if num_words >= 150:
+        text_dtm = imported_vect.transform(text_arr)
+        res = imported_model.predict(text_dtm)[0]
+
+    return res
