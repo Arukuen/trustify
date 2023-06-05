9c7e98e38106311072ad70a6af685e79c19f3946
Gabriel Samonte <samontegabriel2001@gmail.com>
2023-06-05 23:02:02 +0800

    Documented main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ class Text(BaseModel):
 
 app = FastAPI()
 
+# Route for Testing Purposes
 @app.get("/")
 def is_alive():
     return {"is_alive": True}
 
+# Route for the actual API
 @app.post("/api/text/")
 async def process_text(text: Text):
-    is_fake = model_api.predict(text.data)
-    response_data = {"is_fake": is_fake}
-    return response_data
+    is_fake = model_api.predict(text.data) # call model_api.predict
+    response_data = {"is_fake": is_fake} # create response
+    return response_data # respond to HTTP POST request
