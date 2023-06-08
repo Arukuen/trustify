2b032c827ea03dfbdd56bbd94499b6bf0c520ce2
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-06-08 22:59:51 +0800

    feat: add additional features' model and prediction

--- model_api.py
+++ model_api.py
@@ -10,6 +10,16 @@ with open('./model/model_pickle', 'rb') as f:
 with open('./model/vect_pickle', 'rb') as f:
     imported_vect = pickle.load(f)
 
+with open('./model/model_pickle_svm', 'rb') as f:
+    imported_model_svm = pickle.load (f)
+with open('./model/vect_pickle_svm', 'rb') as f:
+    imported_vect_svm = pickle.load (f)
+
+with open('./model/model_pickle_logistic_regression', 'rb') as f:
+    imported_model_logistic_regression = pickle.load (f)
+with open('./model/vect_pickle_logistic_regression', 'rb') as f:
+    imported_vect_logistic_regression = pickle.load (f)
+
 def predict(text):
     # add text to an array
     text_arr = []
@@ -22,12 +32,16 @@ def predict(text):
     # res = 1 -> fake
     # res = 2 -> invalid
 
-    res = 2 # default is invalid
+    res = [2, 2, 2] # default is invalid
 
     # if valid
     if num_words >= 150:
         # run input to the vectorizer and model
-        text_dtm = imported_vect.transform(text_arr)
-        res = int(imported_model.predict(text_dtm)[0])
+        text_dtm_nb = imported_vect.transform(text_arr)
+        text_dtm_svm = imported_vect_svm.transform(text_arr)
+        text_dtm_logistic_regression = imported_vect_logistic_regression.transform(text_arr)
+        res = [int(imported_model.predict(text_dtm_nb)[0]),
+               int(imported_model.predict(text_dtm_svm)[0]),
+               int(imported_model.predict(text_dtm_logistic_regression)[0])]
 
     return res
